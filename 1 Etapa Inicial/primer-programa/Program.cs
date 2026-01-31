b4c9eda017d0903f183bf52a67321db1265b3624
Yamila <fernandezyamila075@gmail.com>
2026-01-31 20:59:44 -0300

    KAN-4 Initial project setup

---
+++ 1 Etapa Inicial/primer-programa/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace primer_programa;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    //Inicio Programa
+        Console.WriteLine("Hello, soy Yamiii!");
+        Console.WriteLine("Mi Primer Programa en C#");
+    //Fin Programa
+    }
+}
+
+// dotnet new console -o nombreprograma --use-program-main
---
+++ 2 Primer Programa/Calculadora/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Calculadora;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Paso 0: Declarar variables con tipo de DATO.
+        int n1, n2;
+
+        // Paso 1: Pedir valores.
+        Console.WriteLine("Ingrese un número: ");
+        Console.ReadLine();
+        Console.WriteLine("Ingrese otro número: ");
+        Console.ReadLine();
+        // Paso 2: Realizar Cálculo.
+
+        // Paso 3: Emitir resultado.
+    }
+}
